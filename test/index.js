22f848b9c0dbd6faf031937e36cb02ed624af99e
Ricardo Nogueira <rgn@seegno.com>
2016-05-18 00:50:57 +0100

    Add MySQL client tests
     - Update travis configuration
     - Remove test fixtures
     - Add MySQL knexfile

--- test/index.js
+++ test/index.js
@@ -3,165 +3,335 @@
  * Module dependencies.
  */
 
+import { clearTables, dropTables, fixtures, recreateTables } from './utils';
+import Bookshelf from 'bookshelf';
+import cascadeDelete from '../src';
+import knex from 'knex';
+import mysqlKnexfile from './mysql.knexfile';
+import postgresKnexfile from './postgres.knexfile';
 import should from 'should';
 import sinon from 'sinon';
-import { Account, Author, Comment, Post } from './fixtures/models';
-import { Accounts, Authors, Comments, Posts } from './fixtures/collections';
-import { Model, repository } from './fixtures/repository';
-import { clearTables, dropTables, recreateTables } from './utils';
 
 /**
  * Test `bookshelf-cascade-delete` plugin.
  */
 
 describe('bookshelf-cascade-delete', () => {
-  before(async () => {
-    await recreateTables();
-  });
+  describe('with PostgreSQL client', () => {
+    const repository = Bookshelf(knex(postgresKnexfile));
+    const Model = repository.Model.prototype;
 
-  beforeEach(async () => {
-    await clearTables();
-  });
+    repository.plugin(cascadeDelete);
 
-  after(async () => {
-    await dropTables();
-  });
+    const { Account, Author, Comment, Post } = fixtures(repository);
 
-  it('should throw an error if model has no registered dependents', async () => {
-    const author = await repository.Model.extend({ tableName: 'Author' }).forge().save();
+    before(async () => {
+      await recreateTables(repository);
+    });
 
-    await Account.forge().save({ authorId: author.get('id') });
+    beforeEach(async () => {
+      await clearTables(repository);
+    });
 
-    try {
+    after(async () => {
+     await dropTables(repository);
+    });
+
+    it('should throw an error if model has no registered dependents', async () => {
+      const author = await repository.Model.extend({ tableName: 'Author' }).forge().save();
+
+      await Account.forge().save({ authorId: author.get('id') });
+
+      try {
+        await author.destroy();
+
+        should.fail();
+      } catch (e) {
+        e.code.should.equal('23503');
+      }
+    });
+
+    it('should throw an error if model has dependents and `cascadeDelete` option is given as `false`', async () => {
+      const author = await Author.forge().save();
+
+      await Account.forge().save({ authorId: author.get('id') });
+
+      try {
+        await author.destroy({ cascadeDelete: false });
+
+        should.fail();
+      } catch (e) {
+        e.code.should.equal('23503');
+      }
+    });
+
+    it('should not delete model and its dependents if an error is thrown on destroy', async () => {
+      const author = await Author.forge().save();
+      const post = await Post.forge().save({ authorId: author.get('id') });
+
+      await Account.forge().save({ authorId: author.get('id') });
+      await Comment.forge().save({ postId: post.get('id') });
+
+      sinon.stub(Model, 'destroy').throws(new Error('foobar'));
+
+      try {
+        await author.destroy();
+
+        should.fail();
+      } catch (e) {
+        e.message.should.equal('foobar');
+      }
+
+      const accounts = await Account.fetchAll();
+      const authors = await Author.fetchAll();
+      const comments = await Comment.fetchAll();
+      const posts = await Post.fetchAll();
+
+      accounts.length.should.equal(1);
+      authors.length.should.equal(1);
+      comments.length.should.equal(1);
+      posts.length.should.equal(1);
+
+      sinon.restore(Model);
+    });
+
+    it('should rollback any query on given `transaction` if an error is thrown on model destroy', async () => {
+      sinon.stub(Model, 'destroy').throws(new Error('foobar'));
+
+      try {
+        await repository.knex.transaction(transaction => Author.forge().save(null, { transacting: transaction })
+          .then(() => Author.forge().save(null, { transacting: transaction }))
+          .then(author => author.destroy({ transacting: transaction }))
+        );
+
+        should.fail();
+      } catch (e) {
+        e.message.should.equal('foobar');
+      }
+
+      const authors = await Author.fetchAll();
+
+      authors.length.should.equal(0);
+
+      sinon.restore(Model);
+    });
+
+    it('should delete model and all its dependents', async () => {
+      const author = await Author.forge().save();
+      const post1 = await Post.forge().save({ authorId: author.get('id') });
+      const post2 = await Post.forge().save({ authorId: author.get('id') });
+
+      await Account.forge().save({ authorId: author.get('id') });
+      await Comment.forge().save({ postId: post1.get('id') });
+      await Comment.forge().save({ postId: post2.get('id') });
+
       await author.destroy();
 
-      should.fail();
-    } catch(e) {
-      e.code.should.equal('23503');
-    }
+      const accounts = await Account.fetchAll();
+      const authors = await Author.fetchAll();
+      const comments = await Comment.fetchAll();
+      const posts = await Post.fetchAll();
+
+      accounts.length.should.equal(0);
+      authors.length.should.equal(0);
+      comments.length.should.equal(0);
+      posts.length.should.equal(0);
+    });
+
+    it('should not delete models which are not dependent', async () => {
+      const author1 = await Author.forge().save();
+      const author2 = await Author.forge().save();
+      const post1 = await Post.forge().save({ authorId: author1.get('id') });
+      const post2 = await Post.forge().save({ authorId: author2.get('id') });
+
+      await Account.forge().save({ authorId: author1.get('id') });
+      await Account.forge().save({ authorId: author2.get('id') });
+      await Comment.forge().save({ postId: post1.get('id') });
+      await Comment.forge().save({ postId: post2.get('id') });
+
+      await author1.destroy();
+
+      const accounts = await Account.fetchAll();
+      const authors = await Author.fetchAll();
+      const comments = await Comment.fetchAll();
+      const posts = await Post.fetchAll();
+
+      accounts.length.should.equal(1);
+      authors.length.should.equal(1);
+      comments.length.should.equal(1);
+      posts.length.should.equal(1);
+    });
+
+    it('should call destroy prototype method with given `options`', async () => {
+      sinon.spy(Model, 'destroy');
+
+      const author = await Author.forge().save();
+
+      await author.destroy({ foo: 'bar' });
+
+      Model.destroy.callCount.should.equal(1);
+      Model.destroy.firstCall.args[0].should.have.properties({ foo: 'bar' });
+
+      sinon.restore(Model);
+    });
   });
 
-  it('should throw an error if model has dependents and `cascadeDelete` option is given as `false`', async () => {
-    const author = await Author.forge().save();
+  describe('with MySQL client', () => {
+    const repository = Bookshelf(knex(mysqlKnexfile));
+    const Model = repository.Model.prototype;
 
-    await Account.forge().save({ authorId: author.get('id') });
+    repository.plugin(cascadeDelete);
 
-    try {
-      await author.destroy({ cascadeDelete: false });
+    const { Account, Author, Comment, Post } = fixtures(repository);
 
-      should.fail();
-    } catch(e) {
-      e.code.should.equal('23503');
-    }
-  });
+    before(async () => {
+      await recreateTables(repository);
+    });
 
-  it('should not delete model and its dependents if an error is thrown on destroy', async () => {
-    const author = await Author.forge().save();
-    const post = await Post.forge().save({ authorId: author.get('id') });
+    beforeEach(async () => {
+      await clearTables(repository);
+    });
 
-    await Account.forge().save({ authorId: author.get('id') });
-    await Comment.forge().save({ postId: post.get('id') });
+    after(async () => {
+     await dropTables(repository);
+    });
 
-    sinon.stub(Model, 'destroy').throws(new Error('foobar'));
+    it('should throw an error if model has no registered dependents', async () => {
+      const author = await repository.Model.extend({ tableName: 'Author' }).forge().save();
 
-    try {
+      await Account.forge().save({ authorId: author.get('id') });
+
+      try {
+        await author.destroy();
+
+        should.fail();
+      } catch (e) {
+        e.errno.should.equal(1451);
+      }
+    });
+
+    it('should throw an error if model has dependents and `cascadeDelete` option is given as `false`', async () => {
+      const author = await Author.forge().save();
+
+      await Account.forge().save({ authorId: author.get('id') });
+
+      try {
+        await author.destroy({ cascadeDelete: false });
+
+        should.fail();
+      } catch (e) {
+        e.errno.should.equal(1451);
+      }
+    });
+
+    it('should not delete model and its dependents if an error is thrown on destroy', async () => {
+      const author = await Author.forge().save();
+      const post = await Post.forge().save({ authorId: author.get('id') });
+
+      await Account.forge().save({ authorId: author.get('id') });
+      await Comment.forge().save({ postId: post.get('id') });
+
+      sinon.stub(Model, 'destroy').throws(new Error('foobar'));
+
+      try {
+        await author.destroy();
+
+        should.fail();
+      } catch (e) {
+        e.message.should.equal('foobar');
+      }
+
+      const accounts = await Account.fetchAll();
+      const authors = await Author.fetchAll();
+      const comments = await Comment.fetchAll();
+      const posts = await Post.fetchAll();
+
+      accounts.length.should.equal(1);
+      authors.length.should.equal(1);
+      comments.length.should.equal(1);
+      posts.length.should.equal(1);
+
+      sinon.restore(Model);
+    });
+
+    it('should rollback any query on given `transaction` if an error is thrown on model destroy', async () => {
+      sinon.stub(Model, 'destroy').throws(new Error('foobar'));
+
+      try {
+        await repository.knex.transaction(transaction => Author.forge().save(null, { transacting: transaction })
+          .then(() => Author.forge().save(null, { transacting: transaction }))
+          .then(author => author.destroy({ transacting: transaction }))
+        );
+
+        should.fail();
+      } catch (e) {
+        e.message.should.equal('foobar');
+      }
+
+      const authors = await Author.fetchAll();
+
+      authors.length.should.equal(0);
+
+      sinon.restore(Model);
+    });
+
+    it('should delete model and all its dependents', async () => {
+      const author = await Author.forge().save();
+      const post1 = await Post.forge().save({ authorId: author.get('id') });
+      const post2 = await Post.forge().save({ authorId: author.get('id') });
+
+      await Account.forge().save({ authorId: author.get('id') });
+      await Comment.forge().save({ postId: post1.get('id') });
+      await Comment.forge().save({ postId: post2.get('id') });
+
       await author.destroy();
 
-      should.fail();
-    } catch(e) {
-      e.message.should.equal('foobar');
-    }
+      const accounts = await Account.fetchAll();
+      const authors = await Author.fetchAll();
+      const comments = await Comment.fetchAll();
+      const posts = await Post.fetchAll();
 
-    const accounts = await Accounts.query();
-    const authors = await Authors.query();
-    const comments = await Comments.query();
-    const posts = await Posts.query();
+      accounts.length.should.equal(0);
+      authors.length.should.equal(0);
+      comments.length.should.equal(0);
+      posts.length.should.equal(0);
+    });
 
-    accounts.length.should.equal(1);
-    authors.length.should.equal(1);
-    comments.length.should.equal(1);
-    posts.length.should.equal(1);
+    it('should not delete models which are not dependent', async () => {
+      const author1 = await Author.forge().save();
+      const author2 = await Author.forge().save();
+      const post1 = await Post.forge().save({ authorId: author1.get('id') });
+      const post2 = await Post.forge().save({ authorId: author2.get('id') });
 
-    sinon.restore(Model);
-  });
+      await Account.forge().save({ authorId: author1.get('id') });
+      await Account.forge().save({ authorId: author2.get('id') });
+      await Comment.forge().save({ postId: post1.get('id') });
+      await Comment.forge().save({ postId: post2.get('id') });
 
-  it('should rollback any query on given `transaction` if an error is thrown on model destroy', async () => {
-    sinon.stub(Model, 'destroy').throws(new Error('foobar'));
+      await author1.destroy();
 
-    try {
-      await repository.knex.transaction(transaction => Author.forge().save(null, { transacting: transaction })
-        .then(() => Author.forge().save(null, { transacting: transaction }))
-        .then(author => author.destroy({ transacting: transaction }))
-      );
+      const accounts = await Account.fetchAll();
+      const authors = await Author.fetchAll();
+      const comments = await Comment.fetchAll();
+      const posts = await Post.fetchAll();
 
-      should.fail();
-    } catch(e) {
-      e.message.should.equal('foobar');
-    }
+      accounts.length.should.equal(1);
+      authors.length.should.equal(1);
+      comments.length.should.equal(1);
+      posts.length.should.equal(1);
+    });
 
-    const authors = await Authors.query();
+    it('should call destroy prototype method with given `options`', async () => {
+      sinon.spy(Model, 'destroy');
 
-    authors.length.should.equal(0);
+      const author = await Author.forge().save();
 
-    sinon.restore(Model);
-  });
+      await author.destroy({ foo: 'bar' });
 
-  it('should delete model and all its dependents', async () => {
-    const author = await Author.forge().save();
-    const post1 = await Post.forge().save({ authorId: author.get('id') });
-    const post2 = await Post.forge().save({ authorId: author.get('id') });
+      Model.destroy.callCount.should.equal(1);
+      Model.destroy.firstCall.args[0].should.have.properties({ foo: 'bar' });
 
-    await Account.forge().save({ authorId: author.get('id') });
-    await Comment.forge().save({ postId: post1.get('id') });
-    await Comment.forge().save({ postId: post2.get('id') });
-
-    await author.destroy();
-
-    const accounts = await Accounts.query();
-    const authors = await Authors.query();
-    const comments = await Comments.query();
-    const posts = await Posts.query();
-
-    accounts.length.should.equal(0);
-    authors.length.should.equal(0);
-    comments.length.should.equal(0);
-    posts.length.should.equal(0);
-  });
-
-  it('should not delete models which are not dependent', async () => {
-    const author1 = await Author.forge().save();
-    const author2 = await Author.forge().save();
-    const post1 = await Post.forge().save({ authorId: author1.get('id') });
-    const post2 = await Post.forge().save({ authorId: author2.get('id') });
-
-    await Account.forge().save({ authorId: author1.get('id') });
-    await Account.forge().save({ authorId: author2.get('id') });
-    await Comment.forge().save({ postId: post1.get('id') });
-    await Comment.forge().save({ postId: post2.get('id') });
-
-    await author1.destroy();
-
-    const accounts = await Accounts.query();
-    const authors = await Authors.query();
-    const comments = await Comments.query();
-    const posts = await Posts.query();
-
-    accounts.length.should.equal(1);
-    authors.length.should.equal(1);
-    comments.length.should.equal(1);
-    posts.length.should.equal(1);
-  });
-
-  it('should call destroy prototype method with given `options`', async () => {
-    sinon.spy(Model, 'destroy');
-
-    const author = await Author.forge().save();
-
-    await author.destroy({ foo: 'bar' });
-
-    Model.destroy.callCount.should.equal(1);
-    Model.destroy.firstCall.args[0].should.have.properties({ foo: 'bar' });
-
-    sinon.restore(Model);
+      sinon.restore(Model);
+    });
   });
 });
